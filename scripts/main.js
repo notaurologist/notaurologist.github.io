f87e6cc8871b4ee63775be8bbb2a94d4cd35ae60
Jason LeMoine <ishotfirst@users.noreply.github.com>
2016-05-08 17:17:40 -0700

    Fancy things up a little bit.

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-6523016-1', 'auto');
+ga('send', 'pageview');
+
+var tol = function(evt) {
+  evt.preventDefault();
+  var url = evt.currentTarget.href;
+  if (!url) return;
+  ga('send', 'event', 'outbound', 'click', url, {
+    'hitCallback': function () {
+      document.location.assign(url);
+    }
+  });
+}
+
+var links = document.querySelectorAll('.outbound');
+for (var i = 0; i < links.length; i++) {
+  links[i].onclick = tol;
+}
